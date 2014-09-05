b470298c616a71266f216dfd1cf985e4de51673a
Juha Syrjälä <juha.syrjala@gmail.com>
2014-09-05 20:29:40 +0300

    More usable LoadGenerator

--- nflow-perf-test/src/main/java/com/nitorcreations/nflow/performance/client/LoadGenerator.java
+++ nflow-perf-test/src/main/java/com/nitorcreations/nflow/performance/client/LoadGenerator.java
@@ -1,24 +1,38 @@
 package com.nitorcreations.nflow.performance.client;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.apache.commons.lang3.time.StopWatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.nitorcreations.nflow.performance.workflow.QuickWorkflow;
 
+@Named
 public class LoadGenerator {
   private static final Logger logger = LoggerFactory.getLogger(LoadGenerator.class);
+  private final StopWatch stopWatch = new StopWatch();
 
+  @Inject
+  private Client client;
+
+  private void generateSomeLoad(int loadCount) {
+    stopWatch.start();
+    for (int i = 0; i < loadCount; i++) {
+      client.createWorkflow(new QuickWorkflow().getType());
+    }
+    logger.info("Generating {} items took {} msec", loadCount, stopWatch.getTime());
+  }
 
   public static void main(String[] args) {
     logger.info("Starting");
 
-    AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(LoadGeneratorConfiguration.class);
-    Client client = ctx.getBean(Client.class);
-    client.createWorkflow(new QuickWorkflow().getType());
-    logger.info("JEE");
-
-    ctx.close();
+    try (AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(LoadGeneratorConfiguration.class)) {
+      LoadGenerator loadGenerator = ctx.getBean(LoadGenerator.class);
+      loadGenerator.generateSomeLoad(20);
+    }
     logger.info("The end");
   }
 }
